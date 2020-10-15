a234c9a9a46649321e02fafa93688aa88b86e044
Bobby Neal <rsneal01@gmail.com>
2020-10-15 17:32:06 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists[1].name
+    self.artists.map do |artist|
+      artist.name
   end
 end
